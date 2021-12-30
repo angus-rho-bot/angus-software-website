7a6925b1f7c10723815a77eaf4dc0836630c7c4c
Harry Cliff <harryacliff@gmail.com>
2021-12-29 20:32:42 -0500

    Created the angussoftwarewebsite package

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,14 +33,13 @@ dependencies {
     //Kotlin Styled (chapter 3)
     implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.3-pre.264-kotlin-1.5.31")
 
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-redux:4.1.0-pre.252-kotlin-1.5.31")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-redux:7.2.4-pre.252-kotlin-1.5.31")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:5.2.0-pre.252-kotlin-1.5.31")
 }
 
 // Heroku Deployment (chapter 9)
 tasks.register("stage") {
     dependsOn("build")
+
 }
 
 rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
