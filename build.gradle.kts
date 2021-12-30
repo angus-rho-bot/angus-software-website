55ce0efa44856238ae248f2ab029c0e99711a06a
Harry Cliff <harryacliff@gmail.com>
2021-12-29 20:47:26 -0500

    Removed unneeded code

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,20 +23,12 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.264-kotlin-1.5.31")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.264-kotlin-1.5.31")
     implementation(npm("react", "17.0.2"))
     implementation(npm("react-dom", "17.0.2"))
 
     implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.3-pre.264-kotlin-1.5.31")
 
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:5.2.0-pre.252-kotlin-1.5.31")
-}
-
-// Heroku Deployment (chapter 9)
-tasks.register("stage") {
-    dependsOn("build")
-
 }
 
 rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
